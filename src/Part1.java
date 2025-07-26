1a6f08f3213050ea2e8befc5f7e29b2110299de7
Chipboong <chipboongte132@gmail.com>
2025-07-27 00:29:38 +0700

    Update Part1.java

--- src/Part1.java
+++ src/Part1.java
@@ -1,8 +1,6 @@
-import edu.duke.*;
-import java.io.*;
 public class Part1{
-		public static int findStopCodon(String dna, int startIndex, String stopCodon) {
-            int start = dna.indexOf("atg");
+    public static int findStopCodon(String dna, int startIndex, String stopCodon) {
+            int start = startIndex;
             int stop = dna.indexOf(stopCodon, startIndex+3);
             if ((start - stop) % 3 == 0) {
                 return stop;
@@ -16,8 +14,37 @@ public class Part1{
         System.out.println(findStopCodon(dna, 0, "taa"));
         System.out.println(dna.substring(0,findStopCodon(dna, 0, "taa")+3));
     }
+    public static String findGene(String dna, int where) {
+        int startIndex = dna.indexOf("atg", where);
+            if (startIndex == -1) {
+                return "";
+            }
+            int stopTaa = findStopCodon(dna, startIndex, "taa");
+            int stopTag = findStopCodon(dna, startIndex, "tag");
+            int stopTga = findStopCodon(dna, startIndex, "tga");
+            int stopCodon = Math.min(stopTga, Math.min(stopTaa, stopTag));
+            if (stopCodon > startIndex && stopCodon < dna.length() + 1) {
+               return dna.substring(startIndex, stopCodon + 3);
+            }
+        return "";
+    }
+    public static void printAllGenes(String dna){
+           int startIndex = 0;
+           int currIndex;
+           while (startIndex != -1) {
+               currIndex = startIndex;
+               String currGene = findGene(dna, currIndex);
+               if (currGene.isEmpty()) {
+                   break;
+               }
+               System.out.println(currGene);
+               startIndex = dna.indexOf(currGene, startIndex + currGene.length());
+               System.out.println(startIndex);
+           }
+           System.out.println(startIndex);
+    }
     public static void main(String[]args){
-            findStopCodon();
-
+        String dna = "atgaaatgaatgtgtaaatgcatgccctaatgcgatagatgtaaatgtgtgaatgccctaaatgaaatgaatgttttagatgaatgaatgtagatgttctaaatgaaaataaatgtgggtgaatgtttttag";
+        printAllGenes(dna);
     }
 }
